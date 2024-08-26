09f971ff0bae060043a76867b5782f2e33d396d3
VuiDev <nguyenminhdao002@gmail.com>
2024-08-26 15:50:11 +0700

    JSTL in view

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,9 +1,8 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
@@ -18,8 +17,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handelHello();
+        model.addAttribute("adam", test);
+        model.addAttribute("toilamdev", "From userController");
         return "hello";
     }
 }
--- src/main/webapp/WEB-INF/view/hello.jsp
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -1,3 +1,6 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,5 +9,7 @@
 </head>
 <body>
     <h1>Hello from JSP</h1>
+    <h1>${adam}</h1>
+    <h2>${toilamdev}</h2>
 </body>
 </html>
