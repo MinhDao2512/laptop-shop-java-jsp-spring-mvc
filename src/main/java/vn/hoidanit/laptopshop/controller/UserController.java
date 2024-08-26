4b26828900d58ea14b494462f26be72fb75444ad
VuiDev <nguyenminhdao002@gmail.com>
2024-08-26 23:08:28 +0700

    send data with HTML Form

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,9 +2,11 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
@@ -26,23 +28,14 @@ public class UserController {
     }
 
     @RequestMapping("/admin/user")
-    public String getCreateUserPage(Model model) {
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User toilamdev) {
+        System.out.println("run here " + toilamdev);
+        return "hello";
+    }
 }
-
-// @RestController
-// public class UserController {
-
-// // DI: Dependency Injection
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("/")
-// public String getHomePage() {
-// return this.userService.handelHello();
-// }
-// }
