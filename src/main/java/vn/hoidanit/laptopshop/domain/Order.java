5b501f17ca54a658770cb9fddd941b7932eaeae2
VuiDev <nguyenminhdao002@gmail.com>
2024-08-28 02:22:10 +0700

    create model for Domain

---
+++ src/main/java/vn/hoidanit/laptopshop/domain/Order.java
@@ -0,0 +1,40 @@
+package vn.hoidanit.laptopshop.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Double totalPrice;
+    // user id
+    // product id
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [id=" + id + ", totalPrice=" + totalPrice + "]";
+    }
+
+}
