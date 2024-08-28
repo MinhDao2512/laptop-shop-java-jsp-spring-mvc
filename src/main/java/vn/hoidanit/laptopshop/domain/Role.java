fbad435baa69e5d69b537ebddd1a28320036b160
VuiDev <nguyenminhdao002@gmail.com>
2024-08-28 09:33:24 +0700

    modified name table

--- src/main/java/vn/hoidanit/laptopshop/domain/Role.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Role.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Roles")
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
