bce55c4712c02d7871769459088c6ecfc31b25ad
VuiDev <nguyenminhdao002@gmail.com>
2024-09-01 22:44:31 +0700

    finish view detail product for client

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ProductController.java
@@ -5,11 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+
 @Controller("clientProductController")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping("/product/{id}")
     public String getDetailPage(Model model, @PathVariable Long id) {
+        Product product = this.productService.getProductById(id);
+        model.addAttribute("product", product);
         return "/client/product/detail";
     }
 }
