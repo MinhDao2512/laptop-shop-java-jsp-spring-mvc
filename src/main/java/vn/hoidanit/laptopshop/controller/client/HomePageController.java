618340a36f9430006b3b0f8b2b2dbc3a3702dd60
VuiDev <nguyenminhdao002@gmail.com>
2024-09-02 23:53:35 +0700

    Mapper class & RegisterDTO class

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -2,20 +2,35 @@ package vn.hoidanit.laptopshop.controller.client;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.Role;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.domain.dto.RegisterDTO;
 import vn.hoidanit.laptopshop.service.ProductService;
+import vn.hoidanit.laptopshop.service.RoleService;
+import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class HomePageController {
 
     private final ProductService productService;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleService roleService;
 
-    public HomePageController(ProductService productService) {
+    public HomePageController(ProductService productService, UserService userService, PasswordEncoder passwordEncoder,
+            RoleService roleService) {
         this.productService = productService;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.roleService = roleService;
     }
 
     @GetMapping("/")
@@ -27,6 +42,23 @@ public class HomePageController {
 
     @GetMapping("/register")
     public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
         return "/client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String postRegisterUser(Model model, @ModelAttribute("registerUser") RegisterDTO registerDTO) {
+        User user = this.userService.registerDTOtoUSer(registerDTO);
+        Role role = this.roleService.getByName("USER");
+
+        user.setPassword(this.passwordEncoder.encode(registerDTO.getPassword()));
+        user.setRole(role);
+        userService.createOrUpdateUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        return "/client/auth/login";
     }
 }
